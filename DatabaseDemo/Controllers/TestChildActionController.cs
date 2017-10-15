16aea422cf1ae2f2fcad5d76217209fa6c7514e0
samer <samer_mail_2006@yahoo.com>
2017-10-15 14:37:14 +0300

    add ChildActionOnly.

---
+++ DatabaseDemo/Controllers/TestChildActionController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DatabaseDemo.Controllers
+{
+    public class TestChildActionController : Controller
+    {
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [ChildActionOnly]
+        [ActionName("CoolMenu")]
+        public ActionResult Menu()
+        {
+            var menu = new List<string>()
+            {
+                "Item 1",
+                "Item 2",
+                "Item 3"
+            };
+            return PartialView("Menu", menu);
+        }
+    }
+}
