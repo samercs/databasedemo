ce6bd118939beb7d1924d9dd9c38c3e4e0bb9496
samer <samer_mail_2006@yahoo.com>
2017-10-15 12:07:20 +0300

    add database init.

--- DatabaseDemo/Global.asax.cs
+++ DatabaseDemo/Global.asax.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using DatabaseDemo.Models;
 
 namespace DatabaseDemo
 {
@@ -12,6 +14,7 @@ namespace DatabaseDemo
     {
         protected void Application_Start()
         {
+            Database.SetInitializer(new DbInitialzer());
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
---
+++ DatabaseDemo/Models/DbInitialzer.cs
@@ -0,0 +1,25 @@
+﻿namespace DatabaseDemo.Models
+{
+    public class DbInitialzer : System.Data.Entity.CreateDatabaseIfNotExists<ApplicationContext>
+    {
+        protected override void Seed(ApplicationContext context)
+        {
+            context.Departments.Add(new Department
+            {
+                Name = "Test Dept 1"
+            });
+            context.Departments.Add(new Department
+            {
+                Name = "Test Dept 2"
+            });
+
+            context.Courses.Add(new Course
+            {
+                Name = "Sport"
+            });
+
+            base.Seed(context);
+
+        }
+    }
+}
